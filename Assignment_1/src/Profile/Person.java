84ee4869248ddde14f973f6b7a5e28478c3a8760
Manthan Wadiwala <wadiwala.m@northeastern.edu>
2021-09-25 21:39:13 -0400

    Initial Commit

---
+++ Assignment_1/src/Profile/Person.java
@@ -0,0 +1,226 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Profile;
+
+/**
+ *
+ * @author manth
+ */
+public class Person {
+    
+   private String name;
+   private String address;
+   private String city; 
+   private String zipCode;
+   private String state;
+   private String country;
+   private String date;
+   private String mobile;   
+   private String fax;      
+   private String email;
+   private String ssn;      
+   private String medicalRecordNo;
+   private String healthPlanNo;    
+   private String bankAcNo;       
+   private String licenseNo;
+   private String vin;
+   private String vehicleLicensePlateNo;
+   private String deviceIdNo;
+
+    public String getDeviceIdNo() {
+        return deviceIdNo;
+    }
+
+    public void setDeviceIdNo(String deviceIdNo) {
+        this.deviceIdNo = deviceIdNo;
+    }
+
+    public String getDeviceSerialNo() {
+        return deviceSerialNo;
+    }
+
+    public void setDeviceSerialNo(String deviceSerialNo) {
+        this.deviceSerialNo = deviceSerialNo;
+    }
+   private String deviceSerialNo;
+   private String linkedinProfile;
+   private String ipAddress;
+   private String uin;
+   private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecordNo() {
+        return medicalRecordNo;
+    }
+
+    public void setMedicalRecordNo(String medicalRecordNo) {
+        this.medicalRecordNo = medicalRecordNo;
+    }
+
+    public String getHealthPlanNo() {
+        return healthPlanNo;
+    }
+
+    public void setHealthPlanNo(String healthPlanNo) {
+        this.healthPlanNo = healthPlanNo;
+    }
+
+    public String getBankAcNo() {
+        return bankAcNo;
+    }
+
+    public void setBankAcNo(String bankAcNo) {
+        this.bankAcNo = bankAcNo;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getVehicleLicensePlateNo() {
+        return vehicleLicensePlateNo;
+    }
+
+    public void setVehicleLicensePlateNo(String vehicleLicensePlateNo) {
+        this.vehicleLicensePlateNo = vehicleLicensePlateNo;
+    }
+
+    public String getLinkedinProfile() {
+        return linkedinProfile;
+    }
+
+    public void setLinkedinProfile(String linkedinProfile) {
+        this.linkedinProfile = linkedinProfile;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getUin() {
+        return uin;
+    }
+
+    public void setUin(String uin) {
+        this.uin = uin;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+   
+   
+
+   
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Wadiwala_Manthan_001099882
+
+Git repository for Assignments of INFO 5100
+
+# Assignment 1
+
+Creation of a user profile and view the created user profile
+
+
+
