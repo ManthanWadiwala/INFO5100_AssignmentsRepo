0ef393077da7b9712607a3049bb229c2a3491061
Manthan Wadiwala <wadiwala.m@northeastern.edu>
2021-11-16 13:24:23 -0500

    Assignment 5 test commit

--- Assigmnent_5/src/Business/Customer/CustomerDirectory.java
+++ Assigmnent_5/src/Business/Customer/CustomerDirectory.java
@@ -5,10 +5,38 @@
  */
 package Business.Customer;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author harold
  */
 public class CustomerDirectory {
     
+    private ArrayList<Customer> customerList;
+
+    public ArrayList<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public void setCustomerList(ArrayList<Customer> customerList) {
+        this.customerList = customerList;
+    }
+    
+    public CustomerDirectory()
+            {
+                this.customerList = new ArrayList<Customer>();
+            }
+
+    public Customer addNewCustomer()
+    {
+        Customer newCustomer = new Customer();
+        customerList.add(newCustomer);
+        return newCustomer;
+    }
+    
+    public void deleteCustomer(int index)
+    {
+        customerList.remove(index);
+    }
 }
